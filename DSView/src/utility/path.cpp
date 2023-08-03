65bd0d3ad68b024071e95e14e1095f7f57cdf756
Yangff <yangff1@gmail.com>
2023-08-03 00:59:06 -0500

    fix codec

--- DSView/src/utility/path.cpp
+++ DSView/src/utility/path.cpp
@@ -49,7 +49,7 @@ namespace path{
         std::string str;
          
 #ifdef _WIN32
-        QTextCodec *codec = QTextCodec::codecForName("System");
+        QTextCodec *codec = QTextCodec::codecForLocale();
         if (codec != NULL){
             QByteArray str_tmp = codec->fromUnicode(path);
             str = str_tmp.data();
